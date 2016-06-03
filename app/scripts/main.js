c2bb3a674a7596d636df381555411e48fecbfe83
Julian Perez <julianperez@strategee.us>
2016-06-03 08:49:49 -0500

    Menu small fijo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+
+$('.main-header__menu-xs').on("click",function(){
+  $('.menu__small').toggleClass('hidden-xs');
+    if ($('.menu__small').hasClass('hidden-xs')) {
+    } else {
+      $('.menu__small').removeClass('main_disable');
+      $('.menu__small').addClass('main_enable');
+    }
+})
